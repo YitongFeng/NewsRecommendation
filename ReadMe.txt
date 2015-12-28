620b1c45632c334c775d4ca8ca7ffa8b0b485412
fyt <imfengyitong@hotmail.com>
2015-12-28 17:38:14 +0800

    1. use ordinary mode: jieba.cut(sentences) 2. sorted news_id .etc files, remove duplicated news 3. add user_reads files 4. Total number of words has been cut down to 22186 (over 10million in last version)

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,31 +1,28 @@
 测试集以test开头，训练集以train开头。
+——————————————————————————————————————————————————————————————————————————————————————————
 
 1. stopword.txt为停用此表
 
-2. test_body_tf.txt 为测试集新闻正文的词频统计 每一行为一条string 各个词与统计数字以空格分离 与
+2. test.txt 为测试用的数据
+
+3. words.txt 不论是测试集还是训练集 新闻标题还是新闻正文 所有出现过的词（已去停词 去重 保留了df > 5 && df < 500 的词）
+——————————————————————————————————————————————————————————————————————————————————————————
+
+4. test_body_tf.txt 为测试集新闻正文的词频统计 每一行为一条string 各个词与统计数字以空格分离 与
 test_news_ids相对应
 
-3. test_news_body.txt 为测试集新闻正文 每一行为一条新闻正文 与test_news_ids相对应
-
-4. test_news_ids.txt 为测试集新闻id
-
-5. test_news_title.txt 为测试集新闻标题 一行一个标题 与test_news_ids相对应
+5. test_news_ids.txt 为测试集新闻id
 
 6. test_title_tf.txt 测试集新闻标题词频统计 每一行为一条string 各个词与统计数字以空格分离 与
 test_news_ids相对应
 
-7. test_user_ids 测试集用户id
+7. test_user_reads 测试集用户浏览的新闻id 用户与新闻id之间以两个“\t\t”间隔 新闻id之间以“ ”间隔
+——————————————————————————————————————————————————————————————————————————————————————————
 
 8. train_body_tf.txt 训练集新闻正文的词频统计 每一行为一条string 各个词与统计数字以空格分离 与train_news_ids相对应
 
-9. train_news_body.txt 训练集新闻正文 每一行为一条新闻正文 与train_news_ids相对应
+9. train_news_ids.txt 训练集新闻id
 
-10. train_news_ids.txt 训练集新闻id
+10. train_title_tf.txt 训练集新闻标题词频统计 每一行为一条string 各个词与统计数字以空格分离 与train_news_ids相对应
 
-11. train_news_title.txt 训练集新闻标题 一行一个标题 与train_news_ids相对应
-
-12. train_title_tf.txt 训练集新闻标题词频统计 每一行为一条string 各个词与统计数字以空格分离 与train_news_ids相对应
-
-13. train_user_ids 训练集用户id
-
-14. words.txt 不论是测试集还是训练集 新闻标题还是新闻正文 所有出现过的词（已去停词 去重）
+11. train_user_reads 训练集用户浏览的新闻id 用户id与新闻id之间以两个“\t\t”间隔 新闻id之间以“ ”间隔
--- recommendation.py
+++ recommendation.py
@@ -9,33 +9,24 @@ import re
 import jieba
 
 
-train_user_ids = []
 train_news_ids = []
-train_read_time = []
-train_news_title = []
-train_news_body = []
-train_publish_time = []
 train_title_tf = []
 train_body_tf = []
-train_title_tf = []
-train_body_tf = []
+train_user_reads = {}
 
-test_user_ids = []
 test_news_ids = []
-test_read_time = []
-test_news_title = []
-test_news_body = []
-test_publish_time = []
 test_title_tf = []  
 test_body_tf = []
-test_title_tf = []
-test_body_tf = []
+test_user_reads = {}
+
+word_df_dic = {}
+
 
 def data_process(filename):
+    global stopkeys
     stopkeys = [line.strip() for line in open('stopword.txt').readlines()]     #stop key table
     t = (2014, 3, 20, 0, 0, 0, 3, 79, 0)    #2014.3.20 struct time
     stamp_time = time.mktime(t)      #convert to time stamp of Unix
-    words = set()       # store the total word appered in news
 
     try:
         with open(filename, 'r') as f:
@@ -43,98 +34,62 @@ def data_process(filename):
                 sp = line.split('\t')
 
                 #filter out the chinese with regex
-                print('filter out the chinese with regex')
+                #print('filter out the chinese with regex')
                 regex = u"[\u4e00-\u9fa5]+"
                 titles = re.findall(regex, sp[3])
                 bodies = re.findall(regex, sp[4])
 
-                #word segmentation
-                print('word segmentation & remove stop keys')
-                title_cuts = []
-                for title in titles:
-                    title_cuts.extend(list(jieba.cut_for_search(title, HMM=True)))
-                body_cuts = []
-                for body in bodies:
-                    body_cuts.extend(list(jieba.cut_for_search(body, HMM=True)))
-
-                #remove stop keys
-                title_cuts_without_stopkeys = [word for word in title_cuts if word not in stopkeys]
-                body_cuts_without_stopkeys = [word for word in body_cuts if word not in stopkeys]
-
-                words = words | set(title_cuts_without_stopkeys) | set(body_cuts_without_stopkeys)
-
-                #compute tf of each news' title and body
-                print('compute tf of each news\' title and body')
-                dic_title_tf = {}
-                for key1 in title_cuts_without_stopkeys:
-                    if key1 in dic_title_tf:
-                        dic_title_tf[key1] = dic_title_tf[key1] + 1
-                    else:
-                        dic_title_tf[key1] = 1
-
-                dic_body_tf = {}
-                for key2 in body_cuts_without_stopkeys:
-                    if key2 in dic_body_tf:
-                        dic_body_tf[key2] = dic_body_tf[key2] + 1
-                    else:
-                        dic_body_tf[key2] = 1
-
                 #divide the dataset
-                print('divide the dataset')
+                #print('divide the dataset')
+
+                # training data set
                 if int(sp[2]) < stamp_time:
-                    train_user_ids.append(sp[0])
-                    train_news_ids.append(sp[1])
-                    train_read_time.append(sp[2])
-                    train_news_title.append(sp[3])
-                    train_news_body.append(str(sp[4]))
-                    train_publish_time.append(sp[5])
+                    if sp[1] not in train_news_ids:
+                        train_news_ids.append(sp[1])
+                        (title_tf_str, body_tf_str) = seg_and_count(titles, bodies)
+                        train_title_tf.append(title_tf_str)
+                        train_body_tf.append(body_tf_str)
 
-                    tmpstr = ""
-                    for k, v in dic_title_tf.items():
-                        tmpstr += k + " " + str(v) + " "
-                    train_title_tf.append(tmpstr)
+                    if sp[0] not in train_user_reads:
+                        train_user_reads[sp[0]] = str(sp[1])
+                    else:
+                        train_user_reads[sp[0]] += " " + str(sp[1])
 
-                    tmpstr = ""
-                    for k, v in dic_body_tf.items():
-                        tmpstr += k + " " + str(v) + " "
-                    train_body_tf.append(tmpstr)
 
+                # test data set
                 else:
-                    test_user_ids.append(sp[0])
-                    test_news_ids.append(sp[1])
-                    test_read_time.append(sp[2])
-                    test_news_title.append(sp[3])
-                    test_news_body.append(str(sp[4]))
-                    test_publish_time.append(sp[5])
+                    if sp[1] not in test_news_ids:
+                        test_news_ids.append(sp[1])
+                        (title_tf_str, body_tf_str) = seg_and_count(titles, bodies)
+                        test_title_tf.append(title_tf_str)
+                        test_body_tf.append(body_tf_str)
+                    if sp[0] not in train_user_reads:
+                        train_user_reads[sp[0]] = str(sp[1])
+                    else:
+                        train_user_reads[sp[0]] += " " + str(sp[1])
 
-                    tmpstr = ""
-                    for k, v in dic_title_tf.items():
-                        tmpstr += k + " " + str(v) + " "
-                    test_title_tf.append(tmpstr)
 
-                    tmpstr = ""
-                    for k, v in dic_body_tf.items():
-                        tmpstr += k + " " + str(v) + " "
-                    test_body_tf.append(tmpstr)
 
         with open('words.txt', 'w') as f:
-            f.writelines('\t'.join(list(words)))
-        with open('train_user_ids.txt', 'w') as f:
-            f.writelines('\n'.join(train_user_ids))
-        with open('test_user_ids.txt', 'w') as f:
-            f.writelines('\n'.join(test_user_ids))
+            # filter out the words according to df
+            word_number = 0;
+            for k, v in word_df_dic.items():
+                if v < 500 and v > 5:
+                    f.write(k + " " + str(v) + "\n")
+                    word_number += 1;
+                    print(word_number)
+
+        with open('train_user_reads.txt', 'w') as f:
+            for k, v in train_user_reads.items():
+                f.write(k + "\t\t" + v + "\n")
+        with open('test_user_reads.txt', 'w') as f:
+            for k, v in test_user_reads.items():
+                f.write(k + "\t\t" + v + "\n")
         with open('train_news_ids.txt', 'w') as f:
             f.writelines('\n'.join(train_news_ids))
         with open('test_news_ids.txt', 'w') as f:
             f.writelines('\n'.join(test_news_ids))
-        with open('train_news_title.txt', 'w') as f:
-            f.writelines('\n'.join(train_news_title))
-        with open('test_news_title.txt', 'w') as f:
-            f.writelines('\n'.join(test_news_title))
-        with open('train_news_body.txt', 'w') as f:
-            f.writelines('\n'.join(train_news_body))
-        with open('test_news_body.txt', 'w') as f:
-            f.writelines('\n'.join(test_news_body))
+
         with open('train_title_tf.txt', 'w') as f:
             f.writelines('\n'.join(train_title_tf))
         with open('test_title_tf.txt', 'w') as f:
@@ -144,10 +99,65 @@ def data_process(filename):
         with open('test_body_tf.txt', 'w') as f:
             f.writelines('\n'.join(test_body_tf))
 
+
+
     except IOError as ioerr:
         print('File Error' + str(ioerr))    #print the error
         return None
 
+def seg_and_count(titles, bodies):
+    #word segmentation
+    #count the df of each word
+    #count the tf of titles and bodies
 
+    words = set()       # store the total word appered in news
+
+    #word segmentation
+    #print('word segmentation & remove stop keys')
+    title_cuts = []
+    for title in titles:
+        title_cuts.extend(list(jieba.cut(title)))
+    body_cuts = []
+    for body in bodies:
+        body_cuts.extend(list(jieba.cut(body)))
+
+    #remove stop keys
+    title_cuts_without_stopkeys = [word for word in title_cuts if len(word) >= 2 and word not in stopkeys]
+    body_cuts_without_stopkeys = [word for word in body_cuts if len(word) >= 2 and word not in stopkeys]
+    # use set to remove repeated words
+    set_title_cuts = set(title_cuts_without_stopkeys)
+    set_body_cuts = set(body_cuts_without_stopkeys)
+    words = words | set_title_cuts | set_body_cuts
+    # compute df of each word
+    for w in words:
+        if w in word_df_dic:
+            word_df_dic[w] += 1
+        else:
+            word_df_dic[w] = 1
+
+    #compute tf of each news' title and body
+    #print('compute tf of each news\' title and body')
+    dic_title_tf = {}
+    for key1 in title_cuts_without_stopkeys:    # compute title-tf can't use set_title_cuts
+        if key1 in dic_title_tf:
+            dic_title_tf[key1] += 1
+        else:
+            dic_title_tf[key1] = 1
+
+    dic_body_tf = {}
+    for key2 in body_cuts_without_stopkeys:
+        if key2 in dic_body_tf:
+            dic_body_tf[key2] = dic_body_tf[key2] + 1
+        else:
+             dic_body_tf[key2] = 1
+
+    title_tf_str = ""
+    for k, v in dic_title_tf.items():
+        title_tf_str += k + " " + str(v) + " "
+    body_tf_str = ""
+    for k, v in dic_body_tf.items():
+        body_tf_str += k + " " + str(v) + " "
+
+    return (title_tf_str, body_tf_str)
 
 print(sys.getdefaultencoding())
